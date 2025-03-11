7742239d4dda6c91e47e6c782cb1ac7415c1d2cc
tiivikofficial <tiivikofficial@gmail.com>
2025-03-11 22:40:19 +0330

    Add files via upload

---
+++ config.example.py
@@ -0,0 +1,6 @@
+# Example configuration file
+# Rename this file to config.py and enter your credentials
+
+API_ID = 'YOUR_API_ID'  # Get this from https://my.telegram.org
+API_HASH = 'YOUR_API_HASH'  # Get this from https://my.telegram.org
+PHONE_NUMBER = 'YOUR_PHONE_NUMBER'  # Full phone number with country code
